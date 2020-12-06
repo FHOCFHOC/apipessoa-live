ae5b63f307e794d2eb03f7a4c6068b6ccc77b484
FHOCFHOC <backintheusa@gmail.com>
2020-12-06 19:35:40 -0300

    commit inicial

---
+++ src/main/java/one/digitallinnovation/apipessoa/ApipessoaApplication.java
@@ -0,0 +1,13 @@
+package one.digitallinnovation.apipessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipessoaApplication.class, args);
+	}
+
+}
