c4d1997eae17894b1bff545b5cb9ab103976dedb
FHOCFHOC <backintheusa@gmail.com>
2020-12-07 15:44:04 -0300

    quarto commit

--- src/main/java/one/digitallinnovation/apipessoa/controller/PessoaController.java
+++ src/main/java/one/digitallinnovation/apipessoa/controller/PessoaController.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/pessoa")
-//@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PessoaController {
 
     private PessoaService pessoaService;
 
-    public PessoaController(PessoaService pessoaService){
+    public PessoaController(PessoaService pessoaService) {
         this.pessoaService = pessoaService;
     }
 
@@ -25,27 +24,4 @@ public class PessoaController {
         return pessoaService.criarPessoa(pessoa);
     }
 
-    //@GetMapping
-   // public String getBook(){
-    //    return "Apir test";
-    }
-    //public List<PersonDTO> listAll() {
-      //  return personService.listAll();
-   // }
-
-    //@GetMapping("/{id}")
-    //public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
-   //     return personService.findById(id);
-   // }
-
-  //  @PutMapping("/{id}")
-   // public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
-    //    return personService.updateById(id, personDTO);
-    //}
-
-   // @DeleteMapping("/{id}")
-   // @ResponseStatus(HttpStatus.NO_CONTENT)
-   // public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
-   //     personService.delete(id);
-   // }
-
+}
